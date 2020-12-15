2435ed4cc51cd28b7c4ee90807e98786a126ab24
Renato L. F. Cunha <renatocunha@acm.org>
2020-12-15 20:46:16 -0300

    ci: try another method to skip python 2.7

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -10,14 +10,15 @@ function repair_wheel {
     fi
 }
 
-# Python 2.7 is too problematic. Drop it
-sudo rm -fr /opt/python/*27*
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements.txt
-    "${PYBIN}/python" -c 'import pathlib' || "${PYBIN}/pip" install pathlib2
-    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    if [[ $PYBIN == *"27"* ]]; then
+        echo 'Ignoring python 2.7.'
+    else
+        "${PYBIN}/pip" install -r /io/requirements.txt
+        "${PYBIN}/python" -c 'import pathlib' || "${PYBIN}/pip" install pathlib2
+        "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    fi
 done
 
 # Bundle external shared libraries into the wheels
